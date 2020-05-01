3e4d68a4cd8549c2a61617db9ded1847a893d6b5
Hakan ÜNAL <hakanunal_1996@hotmail.com>
2020-05-01 22:21:41 +0300

    updated

--- controllers/user/books.js
+++ controllers/user/books.js
@@ -1,5 +1,6 @@
 const logController = require("../log");
 const sessionstorage = require('sessionstorage');
+const database = require("../../utility/database");
 
 
 
@@ -19,29 +20,58 @@ exports.displayBooksPage = (req, res) => {
 
 exports.displayArt = (req, res) => {
     const isAuthen = JSON.parse(sessionstorage.getItem("authentication"));
+    const db = database.getdb();
 
-    if (isAuthen !== true) {
-        res.redirect("/login");
-    } else {
-        res.render("./pugs/books/art", { title: "Art", isAuthentication: isAuthen });
-        const date = new Date();
-        logController.saveLocalStorage("GET", date, "/user/books/art")
+
+    const model = (books) => {
+        console.log(books.length);
+
+        if (isAuthen !== true) {
+            res.redirect("/login");
+        } else {
+            res.render("./pugs/books/art", { title: "Art", isAuthentication: isAuthen });
+            const date = new Date();
+            logController.saveLocalStorage("GET", date, "/user/books/art")
+        }
     }
+
+
+    db.collection("books")
+        .find({ weeks_on_list: 7 })
+        .toArray((err, books) => {
+            if (err) {
+            }
+            model(books)
+        })
 }
 
 
 
 exports.displayEducation = (req, res) => {
     const isAuthen = JSON.parse(sessionstorage.getItem("authentication"));
+    const db = database.getdb();
 
 
-    if (isAuthen !== true) {
-        res.redirect("/login");
-    } else {
-        res.render("./pugs/books/education", { title: "Education", isAuthentication: isAuthen });
-        const date = new Date();
-        logController.saveLocalStorage("GET", date, "/user/books/education")
+    const model = (books) => {
+        console.log(books.length);
+
+        if (isAuthen !== true) {
+            res.redirect("/login");
+        } else {
+            res.render("./pugs/books/education", { title: "Education", isAuthentication: isAuthen });
+            const date = new Date();
+            logController.saveLocalStorage("GET", date, "/user/books/education")
+        }
     }
+
+
+    db.collection("books")
+        .find({ weeks_on_list: 8 })
+        .toArray((err, books) => {
+            if (err) {
+            }
+            model(books)
+        })
 }
 
 
@@ -57,21 +87,38 @@ exports.displaySelector = (req, res) => {
         const date = new Date();
         logController.saveLocalStorage("GET", date, "/user/books/selector")
     }
+
 }
 
 
 
 exports.displayBestseller = (req, res) => {
     const isAuthen = JSON.parse(sessionstorage.getItem("authentication"));
+    const db = database.getdb();
 
 
-    if (isAuthen !== true) {
-        res.redirect("/login");
-    } else {
-        res.render("./pugs/books/bestseller", { title: "Bestseller", isAuthentication: isAuthen });
-        const date = new Date();
-        logController.saveLocalStorage("GET", date, "/user/books/bestseller")
+    const model = (books) => {
+        console.log(books.length);
+
+
+        if (isAuthen !== true) {
+            res.redirect("/login");
+        } else {
+            res.render("./pugs/books/bestseller", { title: "Bestseller", isAuthentication: isAuthen });
+            const date = new Date();
+            logController.saveLocalStorage("GET", date, "/user/books/bestseller")
+        }
     }
+
+
+
+    db.collection("books")
+        .find({ weeks_on_list: 25 })
+        .toArray((err, books) => {
+            if (err) {
+            }
+            model(books)
+        })
 }
 
 
@@ -80,60 +127,113 @@ exports.displayBestseller = (req, res) => {
 
 exports.displayPolitics = (req, res) => {
     const isAuthen = JSON.parse(sessionstorage.getItem("authentication"));
+    const db = database.getdb();
 
 
-    if (isAuthen !== true) {
-        res.redirect("/login");
-    } else {
-        res.render("./pugs/books/politics", { title: "Politics", isAuthentication: isAuthen });
-        const date = new Date();
-        logController.saveLocalStorage("GET", date, "/user/books/politics")
+    const model = (books) => {
+        console.log(books.length);
+
+        if (isAuthen !== true) {
+            res.redirect("/login");
+        } else {
+            res.render("./pugs/books/politics", { title: "Politics", isAuthentication: isAuthen });
+            const date = new Date();
+            logController.saveLocalStorage("GET", date, "/user/books/politics")
+        }
     }
+
+    db.collection("books")
+        .find({ weeks_on_list: 10 })
+        .toArray((err, books) => {
+            if (err) {
+            }
+            model(books)
+        })
 }
 
 
 
 exports.displayEconomy = (req, res) => {
     const isAuthen = JSON.parse(sessionstorage.getItem("authentication"));
+    const db = database.getdb();
 
+    const model = (books) => {
+        console.log(books.length);
 
-    if (isAuthen !== true) {
-        res.redirect("/login");
-    } else {
-        res.render("./pugs/books/economy", { title: "Economy", isAuthentication: isAuthen });
-        const date = new Date();
-        logController.saveLocalStorage("GET", date, "/user/books/economy")
+        if (isAuthen !== true) {
+            res.redirect("/login");
+        } else {
+            res.render("./pugs/books/economy", { title: "Economy", isAuthentication: isAuthen });
+            const date = new Date();
+            logController.saveLocalStorage("GET", date, "/user/books/economy")
+        }
     }
+
+
+    db.collection("books")
+        .find({ weeks_on_list: 11 })
+        .toArray((err, books) => {
+            if (err) {
+            }
+            model(books)
+        })
 }
 
 
 
 exports.displayHistory = (req, res) => {
     const isAuthen = JSON.parse(sessionstorage.getItem("authentication"));
+    const db = database.getdb();
 
 
-    if (isAuthen !== true) {
-        res.redirect("/login");
-    } else {
-        res.render("./pugs/books/history", { title: "History", isAuthentication: isAuthen });
-        const date = new Date();
-        logController.saveLocalStorage("GET", date, "/user/books/history")
+    const model = (books) => {
+        console.log(books.length);
+
+        if (isAuthen !== true) {
+            res.redirect("/login");
+        } else {
+            res.render("./pugs/books/history", { title: "History", isAuthentication: isAuthen });
+            const date = new Date();
+            logController.saveLocalStorage("GET", date, "/user/books/history")
+        }
     }
+
+    db.collection("books")
+        .find({ weeks_on_list: 12 })
+        .toArray((err, books) => {
+            if (err) {
+            }
+            model(books)
+        })
 }
 
 
 
 exports.displayChildren = (req, res) => {
     const isAuthen = JSON.parse(sessionstorage.getItem("authentication"));
+    const db = database.getdb();
 
 
-    if (isAuthen !== true) {
-        res.redirect("/login");
-    } else {
-        res.render("./pugs/books/children", { title: "Children", isAuthentication: isAuthen });
-        const date = new Date();
-        logController.saveLocalStorage("GET", date, "/user/books/children")
+    const model = (books) => {
+        console.log(books.length);
+
+        if (isAuthen !== true) {
+            res.redirect("/login");
+        } else {
+            res.render("./pugs/books/children", { title: "Children", isAuthentication: isAuthen });
+            const date = new Date();
+            logController.saveLocalStorage("GET", date, "/user/books/children")
+        }
     }
+
+
+    db.collection("books")
+        .find({ weeks_on_list: 13 })
+        .toArray((err, books) => {
+            if (err) {
+            }
+            model(books)
+        })
 }
 
 
@@ -141,28 +241,54 @@ exports.displayChildren = (req, res) => {
 
 exports.displayCrime = (req, res) => {
     const isAuthen = JSON.parse(sessionstorage.getItem("authentication"));
+    const db = database.getdb();
 
 
-    if (isAuthen !== true) {
-        res.redirect("/login");
-    } else {
-        res.render("./pugs/books/crime", { title: "Crime", isAuthentication: isAuthen });
-        const date = new Date();
-        logController.saveLocalStorage("GET", date, "/user/books/crime")
+    const model = (books) => {
+        console.log(books.length);
+
+        if (isAuthen !== true) {
+            res.redirect("/login");
+        } else {
+            res.render("./pugs/books/crime", { title: "Crime", isAuthentication: isAuthen });
+            const date = new Date();
+            logController.saveLocalStorage("GET", date, "/user/books/crime")
+        }
     }
+
+    db.collection("books")
+        .find({ weeks_on_list: 14 })
+        .toArray((err, books) => {
+            if (err) {
+            }
+            model(books)
+        })
 }
 
 
 
 exports.displayComputer = (req, res) => {
     const isAuthen = JSON.parse(sessionstorage.getItem("authentication"));
+    const db = database.getdb();
 
 
-    if (isAuthen !== true) {
-        res.redirect("/login");
-    } else {
-        res.render("./pugs/books/computer", { title: "Computer", isAuthentication: isAuthen });
-        const date = new Date();
-        logController.saveLocalStorage("GET", date, "/user/books/computer")
+    const model = (books) => {
+        console.log(books.length);
+        if (isAuthen !== true) {
+            res.redirect("/login");
+        } else {
+            res.render("./pugs/books/computer", { title: "Computer", isAuthentication: isAuthen });
+            const date = new Date();
+            logController.saveLocalStorage("GET", date, "/user/books/computer")
+        }
     }
+
+
+    db.collection("books")
+        .find({ weeks_on_list: 15 })
+        .toArray((err, books) => {
+            if (err) {
+            }
+            model(books)
+        })
 }
