04efbfd0cffab64fed1353f4af99b54f815db6c3
Hakan ÜNAL <hakanunal_1996@hotmail.com>
2020-05-03 02:29:08 +0300

    updated

--- controllers/user/events.js
+++ controllers/user/events.js
@@ -1,15 +1,16 @@
 const logController = require("../log");
 const sessionstorage = require('sessionstorage');
-
+const LocalStorage = require('node-localstorage').LocalStorage;
+const localStorage = new LocalStorage('./LocalStorage');
 
 
 exports.displayEventsPage = (req, res) => {
     const isAuthen = JSON.parse(sessionstorage.getItem("authentication"));
-
+    const events = JSON.parse(localStorage.getItem("etkinlikler"));
     if (isAuthen !== true) {
         res.redirect("/login");
     } else {
-        res.render("./pugs/events", { title: "Events", isAuthentication: isAuthen });
+        res.render("./pugs/events", { title: "Events", isAuthentication: isAuthen, events: events });
         const date = new Date();
         logController.saveLocalStorage("GET", date, "/user/events")
     }
---
+++ public/js/events.js
@@ -0,0 +1,8 @@
+const row = document.querySelector("#row");
+const progresBar = document.querySelector("#progresBar");
+
+
+setTimeout(() => {
+    progresBar.setAttribute("class", "d-none");
+    row.setAttribute("class", "row");
+}, 3500);
