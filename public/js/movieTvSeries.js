e5a7173c92aa3bbe7be81a60a8184602cbbed2a5
Hakan ÜNAL <hakanunal_1996@hotmail.com>
2020-05-04 02:27:16 +0300

    updated

--- public/js/movieTvSeries.js
+++ public/js/movieTvSeries.js
@@ -5,11 +5,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const type = document.querySelector("#type");
     const platform = document.querySelector("#platform");
     const row = document.querySelector("#row");
-
+    const content = document.querySelector("#content");
 
 
     const getData = () => {
-        fetch('http://localhost:3000/json/filmler_ve_diziler.json')
+
+        return fetch('http://localhost:3000/json/filmler_ve_diziler.json')
             .then(response => {
                 return response.json()
             }).then(text => {
@@ -42,6 +43,48 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+    const displayList = (category, type, platform) => {
+        
+
+        getData().then(data => {
+
+            content.innerHTML = `
+            <table class="table table-dark">
+                <thead>
+                <tr>
+                    <th scope="col">#Poster</th>
+                    <th scope="col">#Name</th>
+                    <th scope="col">#Platform</th>
+                    <th scope="col">#Category</th>
+                    <th scope="col">#Type</th>
+                </tr>
+                <tbody id="table">
+                </tbody>
+            </table>
+            `;
+            
+            data.forEach(element => {
+                if (element.ktg1 === category & element.type === type & element.platform === platform) {
+                    document.querySelector("#table").innerHTML += `
+                <tr>
+                    <th> <a href="${element.url}"> <img src="${element.img}" style="width:100px;"> </a> </th>
+                    <th> ${element.name}</th>
+                    <th>${element.platform}</th>
+                    <th> ${element.ktg1}</th>
+                    <th>${element.type}</th>
+                </tr>
+
+            `
+                }
+            });
+
+
+
+        });
+    }
+
+
+
     const progresBarLoading = (category, type, platform) => {
         row.setAttribute("class", "row");
         progresBarAnimation();
@@ -58,18 +101,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     category.addEventListener("input", () => {
+        content.innerHTML="";
+        console.log(category.value, type.value, platform.value);
+        console.log("category")
+
         progresBarLoading(category.value, type.value, platform.value);
     })
 
 
 
     type.addEventListener("input", () => {
+        content.innerHTML="";
+
+        console.log(category.value, type.value, platform.value);
+        console.log("type")
+
         progresBarLoading(category.value, type.value, platform.value);
     })
 
 
 
     platform.addEventListener("input", () => {
+        content.innerHTML="";
+
+        console.log(category.value, type.value, platform.value);
+        console.log("platform")
+
         progresBarLoading(category.value, type.value, platform.value);
     })
 
