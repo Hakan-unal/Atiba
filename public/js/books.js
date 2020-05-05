87dd31a97806a199bd9f6386c2eb2d6126e1dd47
Hakan ÜNAL <hakanunal_1996@hotmail.com>
2020-05-05 20:59:21 +0300

    updated

--- public/js/books.js
+++ public/js/books.js
@@ -2,11 +2,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const progresBar = document.querySelector("#progresBar");
     const container = document.querySelector("#contentContainer");
+    const searchInput = document.querySelector("#searchInput");
+    const searchButton = document.querySelector("#searchButton");
+
+
+
 
     container.addEventListener("click", (event) => {
-        if(event.target.className==="card-img-top"){
-            progresBar.setAttribute("class","progress h-100 bg-dark")
+        if (event.target.className === "card-img-top") {
+            progresBar.setAttribute("class", "progress h-100 bg-dark")
         }
     })
+
+
+
+    searchButton.addEventListener("click", () => {
+        if (searchInput.value !== "") {
+            url = `https://www.google.com/search?source=hp&ei=zaexXvzONsSOlwSZ_6ygDA&q=${searchInput.value}&oq=${searchInput.value}`;
+            window.open(url);
+            searchInput.value = "";
+        }
+    })
+
+
+
 
 })
