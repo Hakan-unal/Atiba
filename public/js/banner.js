e17fbb85a908f6a64af7913931009cecb30d215e
Hakan ÜNAL <hakanunal_1996@hotmail.com>
2020-05-06 01:42:25 +0300

    updated

--- public/js/banner.js
+++ public/js/banner.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const banner1 = document.querySelector("#banner1");
     const banner2 = document.querySelector("#banner2");
+    const link1 = document.querySelector("#link1");
+    const link2 = document.querySelector("#link2");
     const url = document.URL;
 
 
@@ -19,20 +21,30 @@ document.addEventListener("DOMContentLoaded", () => {
         const cinemaResult = url.includes(cinemaUrl);
 
         if (booksResult) {
-            banner1.setAttribute("src", `http://localhost:3000/img/banner/${booksUrl}.png`)
-            banner2.setAttribute("src", `http://localhost:3000/img/banner/${booksUrl}.png`)
+            banner1.setAttribute("src", `http://localhost:3000/img/banner/${booksUrl}.jpeg`)
+            banner2.setAttribute("src", `http://localhost:3000/img/banner/${booksUrl}.jpeg`)
+            link1.setAttribute("href", "https://www.kitapyurdu.com/");
+            link2.setAttribute("href", "https://www.kitapyurdu.com/");
         } else if (moviesResult) {
-            banner1.setAttribute("src", `http://localhost:3000/img/banner/${moviesUrl}.png`)
-            banner2.setAttribute("src", `http://localhost:3000/img/banner/${moviesUrl}.png`)
+            banner1.setAttribute("src", `http://localhost:3000/img/banner/${moviesUrl}.jpg`)
+            banner2.setAttribute("src", `http://localhost:3000/img/banner/${moviesUrl}.jpg`)
+            link1.setAttribute("href", "https://www.netflix.com/tr/");
+            link2.setAttribute("href", "https://www.netflix.com/tr/");
         } else if (eventsResult) {
-            banner1.setAttribute("src", `http://localhost:3000/img/banner/${eventsUrl}.png`)
-            banner2.setAttribute("src", `http://localhost:3000/img/banner/${eventsUrl}.png`)
+            banner1.setAttribute("src", `http://localhost:3000/img/banner/${eventsUrl}.jpg`)
+            banner2.setAttribute("src", `http://localhost:3000/img/banner/${eventsUrl}.jpg`)
+            link1.setAttribute("href", "https://www.biletix.com/anasayfa/TURKIYE/en");
+            link2.setAttribute("href", "https://www.biletix.com/anasayfa/TURKIYE/en");
         } else if (cinemaResult) {
-            banner1.setAttribute("src", `http://localhost:3000/img/banner/${cinemaUrl}.png`)
-            banner2.setAttribute("src", `http://localhost:3000/img/banner/${cinemaUrl}.png`)
+            banner1.setAttribute("src", `http://localhost:3000/img/banner/${cinemaUrl}.jpg`)
+            banner2.setAttribute("src", `http://localhost:3000/img/banner/${cinemaUrl}.jpg`)
+            link1.setAttribute("href", "https://www.avsarfilm.com.tr/");
+            link2.setAttribute("href", "https://www.avsarfilm.com.tr/");
         } else {
             banner1.setAttribute("src", "http://localhost:3000/img/banner/all.png")
             banner2.setAttribute("src", "http://localhost:3000/img/banner/all.png")
+            link1.setAttribute("href", "https://www.turhost.com/");
+            link2.setAttribute("href", "https://www.turhost.com/");
         }
 
 
